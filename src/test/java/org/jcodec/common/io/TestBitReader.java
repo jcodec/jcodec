09e9a98d9650b350f568496ddb700665e4fb8436
Alex Zhukov <zhukov.alex@gmail.com>
2015-05-08 19:08:23 -0700

    tests: bitreader

--- src/test/java/org/jcodec/common/io/TestBitReader.java
+++ src/test/java/org/jcodec/common/io/TestBitReader.java
@@ -11,11 +11,46 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
+import net.sourceforge.jaad.aac.AACException;
+import net.sourceforge.jaad.aac.syntax.BitStream;
+import net.sourceforge.jaad.aac.syntax.IBitStream;
+import net.sourceforge.jaad.aac.syntax.NIOBitStream;
+
 import org.jcodec.common.NIOUtils;
 import org.jcodec.common.StringUtils;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestBitReader {
+    @Test
+    public void testEOF() throws Exception {
+        byte[] src = new byte[] { b("10011000") };
+        boolean eof = false;
+        try {
+            BitStream bs = new BitStream(src);
+            for (int i = 0; i < 10; i++) {
+                System.out.println(Integer.toBinaryString(bs.readBits(4)) + " " + bs.getBitsLeft());
+            }
+        } catch (AACException e) {
+            eof = true;
+        }
+        Assert.assertTrue(eof);
+    }
+
+    @Test
+    public void testEOF2() throws Exception {
+        byte[] src = new byte[] { b("10011000") };
+        boolean eof = false;
+        try {
+            IBitStream bs = new NIOBitStream(new BitReader(ByteBuffer.wrap(src)));
+            for (int i = 0; i < 10; i++) {
+                System.out.println(Integer.toBinaryString(bs.readBits(4)) + " " + bs.getBitsLeft());
+            }
+        } catch (AACException e) {
+            eof = true;
+        }
+        Assert.assertTrue(eof);
+    }
 
     @Test
     public void testReader1() throws IOException {
