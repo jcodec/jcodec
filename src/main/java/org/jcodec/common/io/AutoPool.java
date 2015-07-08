e5acaa88eeaa4f1447ac7672ac46d495fe5ebd00
Alex Zhukov <zhukov.alex@gmail.com>
2015-07-08 08:02:22 -0700

    0.1.10z5 - use daemon threads in AutoPool

--- src/main/java/org/jcodec/common/io/AutoPool.java
+++ src/main/java/org/jcodec/common/io/AutoPool.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -20,7 +21,7 @@ public class AutoPool {
     private ScheduledExecutorService scheduler;
 
     private AutoPool() {
-        scheduler = Executors.newScheduledThreadPool(1);
+        scheduler = Executors.newScheduledThreadPool(1, daemonThreadFactory());
         scheduler.scheduleAtFixedRate(new Runnable() {
             public void run() {
                 long curTime = System.currentTimeMillis();
@@ -30,6 +31,18 @@ public class AutoPool {
                 }
             }
         }, 0, 100, TimeUnit.MILLISECONDS);
+    }
+
+    private ThreadFactory daemonThreadFactory() {
+        return new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread t = new Thread(r);
+                t.setDaemon(true);
+                t.setName(AutoPool.class.getName());
+                return t;
+            }
+        };
     }
 
     public static AutoPool getInstance() {
