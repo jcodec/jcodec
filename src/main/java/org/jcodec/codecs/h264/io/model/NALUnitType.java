f588584258f9dd7df876683a0cc8b09c48625f17
Oleksandr Chugai <chugai@gmail.com>
2015-12-14 17:47:26 +0200

    typo

--- src/main/java/org/jcodec/codecs/h264/io/model/NALUnitType.java
+++ src/main/java/org/jcodec/codecs/h264/io/model/NALUnitType.java
@@ -15,7 +15,7 @@ public enum NALUnitType {
     NON_IDR_SLICE(1, "non IDR slice"), SLICE_PART_A(2, "slice part a"), SLICE_PART_B(3, "slice part b"), SLICE_PART_C(
             4, "slice part c"), IDR_SLICE(5, "idr slice"), SEI(6, "sei"), SPS(7, "sequence parameter set"), PPS(8,
             "picture parameter set"), ACC_UNIT_DELIM(9, "access unit delimiter"), END_OF_SEQ(10, "end of sequence"), END_OF_STREAM(
-            11, "end of stream"), FILLER_DATA(12, "filter data"), SEQ_PAR_SET_EXT(13,
+            11, "end of stream"), FILLER_DATA(12, "filler data"), SEQ_PAR_SET_EXT(13,
             "sequence parameter set extension"), AUX_SLICE(19, "auxilary slice");
 
     private final static NALUnitType[] lut;
