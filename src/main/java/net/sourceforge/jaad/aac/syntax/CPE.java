631f72ab914b3800bae817f075a3ca57d0eb0b80
Alex Zhukov <zhukov.alex@gmail.com>
2016-02-25 14:04:52 +0200

    aac enums

--- src/main/java/net/sourceforge/jaad/aac/syntax/CPE.java
+++ src/main/java/net/sourceforge/jaad/aac/syntax/CPE.java
@@ -41,7 +41,7 @@ public class CPE extends Element implements SyntaxConstants {
 			info.decode(_in, conf, commonWindow);
 			icsR.getInfo().setData(info);
 
-			msMask = MSMask.forInt(_in.readBits(2));
+			msMask = CPE.getMSMask(_in.readBits(2));
 			if(msMask.equals(MSMask.TYPE_USED)) {
 				final int maxSFB = info.getMaxSFB();
 				final int windowGroupCount = info.getWindowGroupCount();
@@ -90,4 +90,12 @@ public class CPE extends Element implements SyntaxConstants {
 	public boolean isCommonWindow() {
 		return commonWindow;
 	}
+
+    public static MSMask getMSMask(int i) throws AACException {
+        MSMask[] values = MSMask.values();
+        if (values.length >= i) {
+            throw new AACException("unknown MS mask type");
+        }
+        return values[i];
+    }
 }
--- src/main/java/net/sourceforge/jaad/aac/tools/MSMask.java
+++ src/main/java/net/sourceforge/jaad/aac/tools/MSMask.java
@@ -1,45 +1,14 @@
 package net.sourceforge.jaad.aac.tools;
 
-import net.sourceforge.jaad.aac.AACException;
-
 /**
  * This class is part of JAAD ( jaadec.sourceforge.net ) that is distributed
  * under the Public Domain license. Code changes provided by the JCodec project
  * are distributed under FreeBSD license.
  * 
  * The MSMask indicates, if MS is applied to a specific ICStream.
+ * 
  * @author in-somnia
  */
 public enum MSMask {
-
-	TYPE_ALL_0(0),
-	TYPE_USED(1),
-	TYPE_ALL_1(2),
-	TYPE_RESERVED(3);
-
-	public static MSMask forInt(int i) throws AACException {
-		MSMask m;
-		switch(i) {
-			case 0:
-				m = TYPE_ALL_0;
-				break;
-			case 1:
-				m = TYPE_USED;
-				break;
-			case 2:
-				m = TYPE_ALL_1;
-				break;
-			case 3:
-				m = TYPE_RESERVED;
-				break;
-			default:
-				throw new AACException("unknown MS mask type");
-		}
-		return m;
-	}
-	private int num;
-
-	private MSMask(int num) {
-		this.num = num;
-	}
+    TYPE_ALL_0, TYPE_USED, TYPE_ALL_1, TYPE_RESERVED;
 }
