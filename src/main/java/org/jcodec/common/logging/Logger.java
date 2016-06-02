76d7a530780f8f077eee0ffa322904d443ca061f
Antoine Ramponi <atlassian@ramponi.net>
2016-06-03 00:24:06 +0200

    use of java.util.logging

--- src/main/java/org/jcodec/common/logging/Logger.java
+++ src/main/java/org/jcodec/common/logging/Logger.java
@@ -1,64 +1,34 @@
 package org.jcodec.common.logging;
 
-import java.lang.IllegalStateException;
-import java.lang.StackTraceElement;
-import java.lang.Thread;
-import java.util.LinkedList;
-import java.util.List;
-
 /**
  * This class is part of JCodec ( www.jcodec.org ) This software is distributed
  * under FreeBSD License
- * 
+ * <p/>
  * JCodec has to be dependancy free, so it can run both on Java SE and Android
  * hence defining here our small logger that can be plugged into the logging
  * framework of choice on the target platform
- * 
+ *
  * @author The JCodec project
  */
+
 public class Logger {
 
-    private static List<LogSink> stageSinks = new LinkedList<LogSink>();
-    private static List<LogSink> sinks;
+    private static java.util.logging.Logger logger = java.util.logging.Logger.getLogger("jcodec");
 
     public static void debug(String message) {
-        message(LogLevel.DEBUG, message);
+        logger.fine(message);
     }
 
     public static void info(String message) {
-        message(LogLevel.INFO, message);
+        logger.info(message);
     }
 
     public static void warn(String message) {
-        message(LogLevel.WARN, message);
+        logger.warning(message);
     }
 
     public static void error(String message) {
-        message(LogLevel.ERROR, message);
+        logger.severe(message);
     }
 
-    private static void message(LogLevel level, String message) {
-        if (sinks == null) {
-            synchronized (Logger.class) {
-                if (sinks == null) {
-                    sinks = stageSinks;
-                    stageSinks = null;
-                    if (sinks.isEmpty())
-                        sinks.add(OutLogSink.createOutLogSink());
-                }
-            }
-        }
-        StackTraceElement tr = Thread.currentThread().getStackTrace()[3];
-        Message msg = new Message(level, tr.getFileName(), tr.getClassName(), tr.getMethodName(), tr.getLineNumber(),
-                message);
-        for (LogSink logSink : sinks) {
-            logSink.postMessage(msg);
-        }
-    }
-
-    public static void addSink(LogSink sink) {
-        if (stageSinks == null)
-            throw new IllegalStateException("Logger already started");
-        stageSinks.add(sink);
-    }
 }
