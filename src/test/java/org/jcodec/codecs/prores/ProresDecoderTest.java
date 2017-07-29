a6655b008385fdf19eb1c8f0067aab2b23d3b0b2
Stanislav Vitvitskiy <stanislav.vitvitskiy@gmail.com>
2017-07-29 13:23:22 +0300

    [prores] Prores high bit depth support

--- src/test/java/org/jcodec/codecs/prores/ProresDecoderTest.java
+++ src/test/java/org/jcodec/codecs/prores/ProresDecoderTest.java
@@ -20,31 +20,37 @@ import org.junit.Test;
 public class ProresDecoderTest {
     public static final String PRORES_PATH = "src/test/resources/test.prores.mov";
     public static final String YUV_FULL_1x_PATH = "src/test/resources/test.prores.d1.yuv";
+    public static final String YUV_FULL_1x_10_PATH = "src/test/resources/test.prores.yuv";
     public static final String YUV_FULL_2x_PATH = "src/test/resources/test.prores.d2.yuv";
     public static final String YUV_FULL_4x_PATH = "src/test/resources/test.prores.d4.yuv";
     public static final String YUV_FULL_8x_PATH = "src/test/resources/test.prores.d8.yuv";
 
     @Test
     public void testFullSize2Frames() throws IOException {
-        testOneMovie(PRORES_PATH, YUV_FULL_1x_PATH, new ProresDecoder());
+        testOneMovie(PRORES_PATH, YUV_FULL_1x_PATH, new ProresDecoder(), false);
+    }
+    
+    @Test
+    public void testFullSize2Frames10Bit() throws IOException {
+        testOneMovie(PRORES_PATH, YUV_FULL_1x_10_PATH, new ProresDecoder(), true);
     }
     
     @Test
     public void testHalfSize2Frames() throws IOException {
-        testOneMovie(PRORES_PATH, YUV_FULL_2x_PATH, new ProresToThumb4x4());
+        testOneMovie(PRORES_PATH, YUV_FULL_2x_PATH, new ProresToThumb4x4(), false);
     }
     
     @Test
     public void testQuarterSize2Frames() throws IOException {
-        testOneMovie(PRORES_PATH, YUV_FULL_4x_PATH, new ProresToThumb2x2());
+        testOneMovie(PRORES_PATH, YUV_FULL_4x_PATH, new ProresToThumb2x2(), false);
     }
     
     @Test
     public void testQuaverSize2Frames() throws IOException {
-        testOneMovie(PRORES_PATH, YUV_FULL_8x_PATH, new ProresToThumb());
+        testOneMovie(PRORES_PATH, YUV_FULL_8x_PATH, new ProresToThumb(), false);
     }
     
-    public void testOneMovie(String proresPath, String yuvPath, ProresDecoder decoder) throws IOException {
+    public void testOneMovie(String proresPath, String yuvPath, ProresDecoder decoder, boolean hibd) throws IOException {
         SeekableByteChannel inCh = null;
         SeekableByteChannel refCh = null;
         try {
@@ -54,11 +60,16 @@ public class ProresDecoderTest {
             DemuxerTrack videoTrack = demuxer.getVideoTrack();
             VideoCodecMeta meta = videoTrack.getMeta().getVideoCodecMeta();
             Size size = meta.getSize();
-            Picture buffer = Picture.create((size.getWidth() + 15) & ~0xf, (size.getHeight() + 15) & ~0xf,
-                    ColorSpace.YUV422);
+            Picture buffer = Picture.createCropped((size.getWidth() + 15) & ~0xf, (size.getHeight() + 15) & ~0xf,
+                    hibd ? 2 : 0, ColorSpace.YUV422, null);
             for (int i = 0; i < 2; i++) {
                 Packet nextFrame = videoTrack.nextFrame();
-                Picture decoded = decoder.decodeFrame(nextFrame.getData(), buffer.getData());
+                Picture decoded;
+                if (hibd) {
+                    decoded = decoder.decodeFrameHiBD(nextFrame.getData(), buffer.getData(), buffer.getLowBits());
+                } else {
+                    decoded = decoder.decodeFrame(nextFrame.getData(), buffer.getData());
+                }
                 Assert.assertTrue(compare(refCh, decoded));
             }
         } finally {
@@ -70,7 +81,8 @@ public class ProresDecoderTest {
     }
 
     private boolean compare(SeekableByteChannel refCh, Picture decoded) throws IOException {
-        int imgSize = ((decoded.getCroppedWidth() * decoded.getCroppedHeight() * decoded.getColor().bitsPerPixel + 7) >> 3);
+        int imgSize = (((decoded.getCroppedWidth() * decoded.getCroppedHeight() * decoded.getColor().bitsPerPixel + 7) >> 3)) << (decoded
+                .isHiBD() ? 1 : 0);
         ByteBuffer raw = ByteBuffer.allocate(imgSize);
         raw.order(ByteOrder.LITTLE_ENDIAN);
         if (refCh.read(raw) != imgSize)
@@ -80,11 +92,10 @@ public class ProresDecoderTest {
             int width = decoded.getCroppedWidth() >> decoded.getColor().compWidth[comp];
             int height = decoded.getCroppedHeight() >> decoded.getColor().compHeight[comp];
             int stride = decoded.getWidth() >> decoded.getColor().compWidth[comp];
-            byte[] data = decoded.getPlaneData(comp);
             for (int r = 0, ind = 0; r < height; r++) {
                 for (int c = 0; c < width; c++, ind++) {
-                    int refP = raw.get() & 0xff;
-                    int decP = data[ind] + 128;
+                    int refP = getRefPixel(raw, decoded.getLowBitsNum());
+                    int decP = getDecPixel(decoded, comp, ind);
                     if (Math.abs(refP - decP) > 2) {
                         System.out.println("ref[" + c + ", " + r + "] != dec[" + c + ", " + r + "] (" + refP + ","
                                 + decP + ")");
@@ -95,5 +106,14 @@ public class ProresDecoderTest {
             }
         }
         return true;
+    }
+
+    private int getDecPixel(Picture decoded, int comp, int ind) {
+        int pix = decoded.getPlaneData(comp)[ind] + 128;
+        return decoded.isHiBD() ? (pix << 2) + decoded.getLowBits()[comp][ind] : pix;
+    }
+
+    private int getRefPixel(ByteBuffer raw, int hiBits) {
+        return hiBits > 0 ? (raw.getShort() & 0xffff) : (raw.get() & 0xff);
     }
 }
