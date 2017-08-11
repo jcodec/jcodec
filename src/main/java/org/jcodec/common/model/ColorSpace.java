2a186b88a0bbf5d3d8b20d87c95247ad833168e5
Stanislav Vitvitskiy <stanislav.vitvitskiy@gmail.com>
2017-08-10 19:17:04 -0700

    [xcode] Color labels

--- src/main/java/org/jcodec/common/model/ColorSpace.java
+++ src/main/java/org/jcodec/common/model/ColorSpace.java
@@ -1,5 +1,7 @@
 package org.jcodec.common.model;
 
+import static org.jcodec.common.model.ColorSpace.ANY;
+
 /**
  * 
  * This class is part of JCodec ( www.jcodec.org ) This software is distributed
@@ -24,6 +26,26 @@ public final class ColorSpace {
     public final static ColorSpace GREY = new ColorSpace("GREY", 1, new int[] { 0 }, new int[] { 0 }, new int[] { 0 }, true);
     public final static ColorSpace MONO = new ColorSpace("MONO", 1, _000, _000, _000, true);
     public final static ColorSpace YUV444_10 = new ColorSpace("YUV444_10", 3, _012, _000, _000, true);
+
+    /**
+     * Any color space, used in the cases where any color space will do.
+     */
+    public final static ColorSpace ANY = new ColorSpace("ANY", 0, null, null, null, true);
+    
+    /**
+     * Any planar color space, used in the cases where any planar color space will do.
+     */
+    public final static ColorSpace ANY_PLANAR = new ColorSpace("ANY_PLANAR", 0, null, null, null, true);
+    
+    /**
+     * Any interleaved color space, used in the cases where any interleaved color space will do.
+     */
+    public final static ColorSpace ANY_INTERLEAVED = new ColorSpace("ANY_INTERLEAVED", 0, null, null, null, false);
+    
+    /**
+     * Same color, used in filters to declare that the color stays unchanged.
+     */
+    public final static ColorSpace SAME = new ColorSpace("SAME", 0, null, null, null, false);
 
     public static final int MAX_PLANES = 4;
 
@@ -69,5 +91,35 @@ public final class ColorSpace {
 
     public int getHeightMask() {
         return ~(nComp > 1 ? compHeight[1] : 0);
+    }
+
+    /**
+     * Determines if two colors match. Aside from simply comparing the objects
+     * this function also takes into account lables ANY, ANY_INTERLEAVED, ANY
+     * PLANAR.
+     * 
+     * @param inputColor
+     * @return True if the color is the same or matches the label.
+     */
+    public boolean matches(ColorSpace inputColor) {
+        if (inputColor == this)
+            return true;
+        if (inputColor == ANY || this == ANY)
+            return true;
+        if ((inputColor == ANY_INTERLEAVED || this == ANY_INTERLEAVED || inputColor == ANY_PLANAR || this == ANY_PLANAR)
+                && inputColor.planar == this.planar)
+            return true;
+        return false;
+    }
+
+    /**
+     * Calculates the component size based on the fullt size and color subsampling of the given component index.
+     * @param size
+     * @return Component size
+     */
+    public Size compSize(Size size, int comp) {
+        if (compWidth[comp] == 0 && compHeight[comp] == 0)
+            return size;
+        return new Size(size.getWidth() >> compWidth[comp], size.getHeight() >> compHeight[comp]);
     }
 }
