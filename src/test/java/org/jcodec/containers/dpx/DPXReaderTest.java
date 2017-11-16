d605f3e3b0ce622bf8307259586623d5b4ac277f
Anton P. Linevich <anton@linevich.com>
2017-11-16 12:08:51 +0200

    try to handle iso8601 dates more carefully

--- src/test/java/org/jcodec/containers/dpx/DPXReaderTest.java
+++ src/test/java/org/jcodec/containers/dpx/DPXReaderTest.java
@@ -1,27 +1,55 @@
 package org.jcodec.containers.dpx;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import java.io.File;
+import java.io.IOException;
 
 import org.jcodec.common.io.IOUtils;
 import org.jcodec.common.io.NIOUtils;
 import org.jcodec.common.io.SeekableByteChannel;
-import org.junit.Assert;
 import org.junit.Test;
 
 public class DPXReaderTest {
 
-    private final String DPX_HEADER_PATH = "src/test/resources/dpx/3k-0086400.dpx";
+    private final String DPX_V2 = "src/test/resources/dpx/3k-0086400.dpx";
+    private final String DPX_V1 = "src/test/resources/dpx/3k-MACG_S02_Ep040_ING_5893876_00000.dpx";
 
     @Test
-    public void parseMetadata() throws Exception {
-        SeekableByteChannel _in = NIOUtils.readableChannel(new File(DPX_HEADER_PATH));
+    public void tryParseISO8601Date() {
+        assertNotNull(DPXReader.tryParseISO8601Date("2017:09:05:14:14:59:-08"));
+        assertNotNull(DPXReader.tryParseISO8601Date("2017:09:05:14:14:59:-008"));
+        assertNotNull(DPXReader.tryParseISO8601Date("2017:09:05:14:14:59:-0008"));
+    }
+
+    @Test
+    public void parseMetadata_DPXV1() throws IOException {
+        SeekableByteChannel _in = NIOUtils.readableChannel(new File(DPX_V1));
 
         try {
             DPXReader dpx = new DPXReader(_in);
             DPXMetadata dpxMetadata = dpx.parseMetadata();
             String timecodeString = dpxMetadata.getTimecodeString();
             System.out.println(timecodeString);
-            Assert.assertEquals("01:00:00:00", timecodeString);
+            assertEquals("00:58:30:00", timecodeString);
+            assertEquals(DPXMetadata.V1, dpxMetadata.file.version);
+        } finally {
+            IOUtils.closeQuietly(_in);
+        }
+    }
+
+    @Test
+    public void parseMetadata_DPXV2() throws Exception {
+        SeekableByteChannel _in = NIOUtils.readableChannel(new File(DPX_V2));
+
+        try {
+            DPXReader dpx = new DPXReader(_in);
+            DPXMetadata dpxMetadata = dpx.parseMetadata();
+            String timecodeString = dpxMetadata.getTimecodeString();
+            System.out.println(timecodeString);
+            assertEquals("01:00:00:00", timecodeString);
+            assertEquals(DPXMetadata.V2, dpxMetadata.file.version);
         } finally {
             IOUtils.closeQuietly(_in);
         }
