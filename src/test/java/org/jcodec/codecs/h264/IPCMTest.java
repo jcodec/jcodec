909aef8fc8670c025698422fefcf661276fe0996
Stan Vitvitskiy <stanislav.vitvitskiy@gmail.com>
2018-01-08 16:03:23 -0800

    [h264dec] IPCM test

---
+++ src/test/java/org/jcodec/codecs/h264/IPCMTest.java
@@ -0,0 +1,31 @@
+package org.jcodec.codecs.h264;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import org.jcodec.Utils;
+import org.jcodec.codecs.h264.io.model.Frame;
+import org.jcodec.codecs.raw.RAWVideoDecoder;
+import org.jcodec.common.io.NIOUtils;
+import org.jcodec.common.model.ColorSpace;
+import org.jcodec.common.model.Picture;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IPCMTest {
+
+    @Test
+    public void testIPCM() throws IOException {
+        H264Decoder h264Decoder = new H264Decoder();
+        ByteBuffer encoded = NIOUtils.fetchFromFile(new File("src/test/resources/h264/ipcm.264"));
+        ByteBuffer raw = NIOUtils.fetchFromFile(new File("src/test/resources/h264/ipcm.yuv"));
+        Picture tmp0 = Picture.create(176, 144, ColorSpace.YUV420);
+        Picture tmp1 = Picture.create(176, 144, ColorSpace.YUV420);
+
+        Frame frame = h264Decoder.decodeFrame(encoded, tmp0.getData());
+        Picture ref = new RAWVideoDecoder(frame.getWidth(), frame.getHeight()).decodeFrame(raw, tmp1.getData());
+        Assert.assertTrue(Utils.picturesRoughlyEqual(ref, frame, 1));
+
+    }
+}
