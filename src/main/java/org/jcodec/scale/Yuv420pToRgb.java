2367de039f758504c0305cb593d942913598166d
Gareth Floodgate <gareth@floodgate.co.uk>
2020-06-04 11:42:48 +0100

    testing slicing

--- src/main/java/org/jcodec/scale/Yuv420pToRgb.java
+++ src/main/java/org/jcodec/scale/Yuv420pToRgb.java
@@ -38,44 +38,142 @@ public class Yuv420pToRgb implements Transform {
 
         int offLuma = 0, offChroma = 0;
         int stride = dst.getWidth();
-        for (int i = 0; i < (dst.getHeight() >> 1); i++) {
-            for (int k = 0; k < (dst.getWidth() >> 1); k++) {
-                int j = k << 1;
-                if (hbd) {
-                    YUV420pToRGBH2H(yh[offLuma + j], yl[offLuma + j], uh[offChroma], ul[offChroma], vh[offChroma],
-                            vl[offChroma], lowBitsNumSrc, data, lowBits, lowBitsNumDst, (offLuma + j) * 3);
-                    YUV420pToRGBH2H(yh[offLuma + j + 1], yl[offLuma + j + 1], uh[offChroma], ul[offChroma],
-                            vh[offChroma], vl[offChroma], lowBitsNumSrc, data, lowBits, lowBitsNumDst,
-                            (offLuma + j + 1) * 3);
-                    YUV420pToRGBH2H(yh[offLuma + j + stride], yl[offLuma + j + stride], uh[offChroma], ul[offChroma],
-                            vh[offChroma], vl[offChroma], lowBitsNumSrc, data, lowBits, lowBitsNumDst,
-                            (offLuma + j + stride) * 3);
-                    YUV420pToRGBH2H(yh[offLuma + j + stride + 1], yl[offLuma + j + stride + 1], uh[offChroma],
-                            ul[offChroma], vh[offChroma], vl[offChroma], lowBitsNumSrc, data, lowBits, lowBitsNumDst,
-                            (offLuma + j + stride + 1) * 3);
-                } else {
-                    YUV420pToRGBN2N(yh[offLuma + j], uh[offChroma], vh[offChroma], data, (offLuma + j) * 3);
-                    YUV420pToRGBN2N(yh[offLuma + j + 1], uh[offChroma], vh[offChroma], data, (offLuma + j + 1) * 3);
+        
+        
+/*
+     	w = 16 old 0 -> 15
+     	slices = 2
+     	new:
+     	  slicew = 16 / slices = 8
+     	  
+     	  0 -> 7 and 8 -> 15
+     	
+     	slicewidth = 8
+     	
 
-                    YUV420pToRGBN2N(yh[offLuma + j + stride], uh[offChroma], vh[offChroma], data,
-                            (offLuma + j + stride) * 3);
-                    YUV420pToRGBN2N(yh[offLuma + j + stride + 1], uh[offChroma], vh[offChroma], data, (offLuma + j
-                            + stride + 1) * 3);
-                }
+		
+*
+*
+*
+*/
+    	int slices = 2;
+//    	int slices = 2;
+    	boolean okToSlice = false;
+    	while (!okToSlice)
+    	{
+    		if (dst.getWidth() % slices == 0)
+    		{
+    			okToSlice = true;
+    			break;
+    		}
+    		
+    		slices = slices / 2;
+    		if (slices == 1)
+    		{
+    			break;
+    		}
+    	}
+    	
+    	if (slices == 1)
+    	{
+    		// skip - threading.
+    	}
 
-                ++offChroma;
-            }
-            if ((dst.getWidth() & 0x1) != 0) {
-                int j = dst.getWidth() - 1;
+		// slicing 
+		
+		int sliceWidth = dst.getWidth() / slices;
 
-                YUV420pToRGBN2N(yh[offLuma + j], uh[offChroma], vh[offChroma], data, (offLuma + j) * 3);
-                YUV420pToRGBN2N(yh[offLuma + j + stride], uh[offChroma], vh[offChroma], data, (offLuma + j + stride) * 3);
+		
+System.out.println("slices = " + slices + " slicewidth: " + sliceWidth + " width: " + dst.getWidth());    		
+        
 
-                ++offChroma;
-            }
+        
+        
+        for (int sliceNo = 0 ; sliceNo < slices ; sliceNo++)
+        {
+        	
+	        for (int i = 0; i < (dst.getHeight() /2 ); i++) {
+	        	for (int k = ((sliceNo * sliceWidth) / 2 ) ; k < ( ((sliceNo+1) * sliceWidth) / 2) ; k++) {
+	
+/* 
+ * k = 0 ; k < 2 ; k+1
+ * k = 0 1
+ * j = 0 2
+ * */
+	        		
+/* 
+ * k = 0 ; k < 4 ; k+2
+ * k = 0 2
+ * 
+ * */
+/*
+ * s = 0 sw = 2
+ * k = 0 * 2 ; k < 1 * 2 ; k+2
+ * k = 0 
+ * 
+ * s = 1 
+ * k = 1 * 2 ; k < 2 * 2 ; k+2
+ * k = 2
+ * 
+ * */	        		
+	        		
 
-            offLuma += 2 * stride;
-        }
+	        		int pretendOffLuma = i * (2 * stride);
+	        		int pretendOffChroma = ((stride / 2) * i) + (k);
+	        		int j = k * 2;
+	        		
+	        		offLuma = pretendOffLuma;
+	        	    offChroma = pretendOffChroma;
+	        		
+	        		if (dst.getWidth() < 10)
+	        		{
+	        			System.out.println("slice: "+ sliceNo +" i: " + i+ " k: " + k + " j: " + j + " offChroma: " + offChroma + " offLuma: " + offLuma + " calc-c: " + pretendOffChroma + " calc-l: " + pretendOffLuma);
+	        		}
+	        		
+	                if (hbd) {
+	                    YUV420pToRGBH2H(yh[offLuma + j], yl[offLuma + j], uh[offChroma], ul[offChroma], vh[offChroma],
+	                            vl[offChroma], lowBitsNumSrc, data, lowBits, lowBitsNumDst, (offLuma + j) * 3);
+	                    YUV420pToRGBH2H(yh[offLuma + j + 1], yl[offLuma + j + 1], uh[offChroma], ul[offChroma],
+	                            vh[offChroma], vl[offChroma], lowBitsNumSrc, data, lowBits, lowBitsNumDst,
+	                            (offLuma + j + 1) * 3);
+	                    YUV420pToRGBH2H(yh[offLuma + j + stride], yl[offLuma + j + stride], uh[offChroma], ul[offChroma],
+	                            vh[offChroma], vl[offChroma], lowBitsNumSrc, data, lowBits, lowBitsNumDst,
+	                            (offLuma + j + stride) * 3);
+	                    YUV420pToRGBH2H(yh[offLuma + j + stride + 1], yl[offLuma + j + stride + 1], uh[offChroma],
+	                            ul[offChroma], vh[offChroma], vl[offChroma], lowBitsNumSrc, data, lowBits, lowBitsNumDst,
+	                            (offLuma + j + stride + 1) * 3);
+	                } else {
+	                    YUV420pToRGBN2N(yh[offLuma + j], uh[offChroma], vh[offChroma], data, (offLuma + j) * 3);
+	                    YUV420pToRGBN2N(yh[offLuma + j + 1], uh[offChroma], vh[offChroma], data, (offLuma + j + 1) * 3);
+	
+	                    YUV420pToRGBN2N(yh[offLuma + j + stride], uh[offChroma], vh[offChroma], data,
+	                            (offLuma + j + stride) * 3);
+	                    YUV420pToRGBN2N(yh[offLuma + j + stride + 1], uh[offChroma], vh[offChroma], data, (offLuma + j
+	                            + stride + 1) * 3);
+	                }
+
+	            }
+	        	
+	        	// TODO - add unit test for odd width and height !!!
+	            if ((dst.getWidth() & 0x1) != 0) {
+	                int j = dst.getWidth() - 1;
+	
+	                YUV420pToRGBN2N(yh[offLuma + j], uh[offChroma], vh[offChroma], data, (offLuma + j) * 3);
+	                YUV420pToRGBN2N(yh[offLuma + j + stride], uh[offChroma], vh[offChroma], data, (offLuma + j + stride) * 3);
+	
+	                ++offChroma;
+	            }
+	        }
+	        
+	        offChroma = 0;
+	        offLuma = 0;
+	    }
+
+        
+        
+// FIXME - values will be wrong here - offChroma and offLuma need to be calcd for max i and max k        
+        
+        
         if ((dst.getHeight() & 0x1) != 0) {
             for (int k = 0; k < (dst.getWidth() >> 1); k++) {
                 int j = k << 1;
