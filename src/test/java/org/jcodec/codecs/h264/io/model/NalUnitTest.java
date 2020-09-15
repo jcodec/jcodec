c6adfa38bfacc2274d0a5a69da9ca75b09aa1b2e
Gareth Floodgate <gareth@floodgate.co.uk>
2020-09-15 18:33:42 +0100

    missed a file in whitespace update ...

--- src/test/java/org/jcodec/codecs/h264/io/model/NalUnitTest.java
+++ src/test/java/org/jcodec/codecs/h264/io/model/NalUnitTest.java
@@ -6,37 +6,35 @@ import java.nio.ByteBuffer;
 
 import org.junit.Test;
 
-public class NalUnitTest 
+public class NalUnitTest
 {
 
     @Test
-    public void testCanReadNalUnitFromValidNal() 
+    public void testCanReadNalUnitFromValidNal()
     {
-    	byte[] data = new byte[] { (byte)0x7C, (byte)0x81  };
-    	
-    	final NALUnit actual = NALUnit.read(ByteBuffer.wrap(data));
-    	
-    	assertEquals("Wrong conversion on valid packet - idc", 3, actual.nal_ref_idc);
-    	assertEquals("Wrong conversion on valid packet - type", NALUnitType.FU_A, actual.type);
+        byte[] data = new byte[] { (byte) 0x7C, (byte) 0x81 };
+
+        final NALUnit actual = NALUnit.read(ByteBuffer.wrap(data));
+
+        assertEquals("Wrong conversion on valid packet - idc", 3, actual.nal_ref_idc);
+        assertEquals("Wrong conversion on valid packet - type", NALUnitType.FU_A, actual.type);
     }
 
-    
+
     @Test
-    public void testCanWriteNalUnitFromValidNal() 
+    public void testCanWriteNalUnitFromValidNal()
     {
-    	byte[] data = new byte[] { (byte)0x7C, (byte)0x81  };
-    	
-    	final NALUnit actual = NALUnit.read(ByteBuffer.wrap(data));
-    	
-    	assertEquals("Wrong conversion on valid packet - idc", 3, actual.nal_ref_idc);
-    	assertEquals("Wrong conversion on valid packet - type", NALUnitType.FU_A, actual.type);
+        byte[] data = new byte[] { (byte) 0x7C, (byte) 0x81 };
 
-    	byte[] data2 = new byte[1];
-    	actual.write(ByteBuffer.wrap(data2));
-    	
-    	assertEquals("wrong data generated", data[0], data2[0]);
+        final NALUnit actual = NALUnit.read(ByteBuffer.wrap(data));
+
+        assertEquals("Wrong conversion on valid packet - idc", 3, actual.nal_ref_idc);
+        assertEquals("Wrong conversion on valid packet - type", NALUnitType.FU_A, actual.type);
+
+        byte[] data2 = new byte[1];
+        actual.write(ByteBuffer.wrap(data2));
+
+        assertEquals("wrong data generated", data[0], data2[0]);
     }
 
-    
-    
 }
