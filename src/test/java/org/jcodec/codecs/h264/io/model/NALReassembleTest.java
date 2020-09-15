dd1fe9fdad5346ee10f20114a331271efaaa2987
Gareth Floodgate <gareth@floodgate.co.uk>
2020-09-15 10:12:41 +0100

    Fix whitespace issues for PR

--- src/test/java/org/jcodec/codecs/h264/io/model/NALReassembleTest.java
+++ src/test/java/org/jcodec/codecs/h264/io/model/NALReassembleTest.java
@@ -9,208 +9,169 @@ import java.util.List;
 
 import org.junit.Test;
 
-
-public class NALReassembleTest 
+public class NALReassembleTest
 {
-	
-	@Test
-	public void testCorrectlyValidtesPointlessInput()
-	{
-		byte[][] data = new byte[][] {  };
-				
-		// Prepare & reassemble - null
-		byte[] out = NALReasemble.defragmentFUANals(null);
 
-		// Prepare & reassemble - empty
-		final List<ByteBuffer> in = assemble(data);
-		out = NALReasemble.defragmentFUANals(in);
-		
-		assertTrue("broken logic", out != null && out.length == 0);
-	}
-	
-	
-	@Test
-	public void testCorrectlyValidtesIncorrecyFirstPacketInput()
-	{
-		byte[][] data1 = new byte[][] { { (byte)0x7D, (byte)0x81 } };
-		byte[][] data2 = new byte[][] { { (byte)0x7C, (byte)0x01 } };
-		byte[][] data3 = new byte[][] { { (byte)0x7C, (byte)0xC1 } };
-				
-		// Prepare & reassemble - bad type - FU-B
-		List<ByteBuffer> in = assemble(data1);
-		byte[] out = NALReasemble.defragmentFUANals(in);
-		
-		assertTrue("broken logic", out != null && out.length == 0);
-		
-		// Prepare & reassemble - bad data, first is NOT start FU
-		in = assemble(data2);
-		out = NALReasemble.defragmentFUANals(in);
-		
-		assertTrue("broken logic", out != null && out.length == 0);
+    @Test
+    public void testCorrectlyValidtesPointlessInput()
+    {
+        byte[][] data = new byte[][] {};
 
-		// Prepare & reassemble - bad type - FU-A START & END
-		in = assemble(data3);
-		out = NALReasemble.defragmentFUANals(in);
-		
-		assertTrue("broken logic", out != null && out.length == 0);
-	}
-	
-	
-	@Test
-	public void testCorrectlyValidtesMultipleStartFUs()
-	{
-		byte[][] data = new byte[][] 
-		{ 
-			new byte[] { (byte)0x7C, (byte)0x81  }, 
-		    new byte[] { (byte)0x7C, (byte)0x81  }
-		};
-				
-		byte[] result = new byte[] 
-		{ 
-			(byte)0xE1 	
-		};
-		
-		// Prepare & reassemble
-		final List<ByteBuffer> in = assemble(data);
-		final byte[] out = NALReasemble.defragmentFUANals(in);
-		
-		assertTrue("broken logic", out != null && out.length > 0);
-		assertArrayEquals("bad reassembly", out, result); 
-	}
-	
-	
-	@Test
-	public void testCorrectlyValidtesMissingEndFU()
-	{
-		byte[][] data = new byte[][] 
-		{ 
-			new byte[] { (byte)0x7C, (byte)0x81  }, 
-		    new byte[] { (byte)0x7C, (byte)0x01  }
-		};
-				
-		byte[] result = new byte[] 
-		{ 
-			(byte)0xE1 	
-		};
-		
-		// Prepare & reassemble
-		final List<ByteBuffer> in = assemble(data);
-		final byte[] out = NALReasemble.defragmentFUANals(in);
-		
-		assertTrue("broken logic", out != null && out.length > 0);
-		assertArrayEquals("bad reassembly", out, result); 
-	}
-	
-	
-	@Test
-	public void testCorrectlyValidtesEndFUBeforeEnd()
-	{
-		byte[][] data = new byte[][] 
-		{ 
-			new byte[] { (byte)0x7C, (byte)0x81, 0x01, 0x02  }, 
-			new byte[] { (byte)0x7C, (byte)0x01, 0x03, 0x04  }, 
-			new byte[] { (byte)0x7C, (byte)0x41, 0x05, 0x06  }, 
-		    new byte[] { (byte)0x7C, (byte)0x41, 0x07, 0x08  }
-		};
-				
-		byte[] result = new byte[] 
-		{ 
-			(byte)0xE1, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 	
-		};
-		
-		// Prepare & reassemble
-		final List<ByteBuffer> in = assemble(data);
-		final byte[] out = NALReasemble.defragmentFUANals(in);
-		
-		assertTrue("broken logic", out != null && out.length > 0);
-		assertArrayEquals("bad reassembly", out, result); 
-	}
-	
-	
-	@Test
-	public void testEmptyFUReassembleWorksStartAndEndOnly()
-	{
-		byte[][] data = new byte[][] 
-		{ 
-			new byte[] { (byte)0x7C, (byte)0x81  }, 
-		    new byte[] { (byte)0x7C, (byte)0x41  }
-		};
-				
-		byte[] result = new byte[] 
-		{ 
-			(byte)0x61 	
-		};
-		
-		// Prepare & reassemble
-		final List<ByteBuffer> in = assemble(data);
-		final byte[] out = NALReasemble.defragmentFUANals(in);
-		
-		assertTrue("broken logic", out != null && out.length > 0);
-		assertArrayEquals("bad reassembly", out, result); 
-	}
-	
-	
-	@Test
-	public void testSimpleReassembleWorksStartAndEndOnly()
-	{
-		byte[][] data = new byte[][] 
-		{ 
-			new byte[] { (byte)0x7C, (byte)0x81, 0x01, 0x02  }, 
-		    new byte[] { (byte)0x7C, (byte)0x41, 0x03, 0x04  }
-		};
-				
-		byte[] result = new byte[] 
-		{ 
-			(byte)0x61, 0x01, 0x02, 0x03, 0x04	
-		};
-		
-		// Prepare & reassemble
-		final List<ByteBuffer> in = assemble(data);
-		final byte[] out = NALReasemble.defragmentFUANals(in);
-		
-		assertTrue("broken logic", out != null && out.length > 0);
-		assertArrayEquals("bad reassembly", out, result); 
-	}
-	
-	
-	@Test
-	public void testMultiPacketReassembleWorksStartAndEndOnly()
-	{
-		byte[][] data = new byte[][] 
-		{ 
-			new byte[] { (byte)0x7C, (byte)0x81, 0x01, 0x02  }, 
-			new byte[] { (byte)0x7C, (byte)0x01, 0x03, 0x04  }, 
-			new byte[] { (byte)0x7C, (byte)0x01, 0x05, 0x06  }, 
-		    new byte[] { (byte)0x7C, (byte)0x41, 0x07, 0x08  }
-		};
-				
-		byte[] result = new byte[] 
-		{ 
-			(byte)0x61, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08	
-		};
-		
-		// Prepare & reassemble
-		final List<ByteBuffer> in = assemble(data);
-		final byte[] out = NALReasemble.defragmentFUANals(in);
-		
-		assertTrue("broken logic", out != null && out.length > 0);
-		assertArrayEquals("bad reassembly", out, result); 
-	}
-	
-	
+        // Prepare & reassemble - null
+        byte[] out = NALReasemble.defragmentFUANals(null);
 
-	private List<ByteBuffer> assemble(final byte[][] data)
-	{
-		// JAVA8: return  Arrays.asList(data).stream().map(b -> ByteBuffer.wrap(b)).collect(Collectors.toList());
-		
-		final List<ByteBuffer> out = new ArrayList<ByteBuffer>(data.length);
-		for (int i = 0 ; i < data.length ; i++)
-		{
-			out.add(ByteBuffer.wrap(data[i]));
-		}
-		
-		return out;
-	}
+        // Prepare & reassemble - empty
+        final List<ByteBuffer> in = assemble(data);
+        out = NALReasemble.defragmentFUANals(in);
 
-	
+        assertTrue("broken logic", out != null && out.length == 0);
+    }
+
+
+    @Test
+    public void testCorrectlyValidtesIncorrecyFirstPacketInput()
+    {
+        byte[][] data1 = new byte[][] { { (byte) 0x7D, (byte) 0x81 } };
+        byte[][] data2 = new byte[][] { { (byte) 0x7C, (byte) 0x01 } };
+        byte[][] data3 = new byte[][] { { (byte) 0x7C, (byte) 0xC1 } };
+
+        // Prepare & reassemble - bad type - FU-B
+        List<ByteBuffer> in = assemble(data1);
+        byte[] out = NALReasemble.defragmentFUANals(in);
+
+        assertTrue("broken logic", out != null && out.length == 0);
+
+        // Prepare & reassemble - bad data, first is NOT start FU
+        in = assemble(data2);
+        out = NALReasemble.defragmentFUANals(in);
+
+        assertTrue("broken logic", out != null && out.length == 0);
+
+        // Prepare & reassemble - bad type - FU-A START & END
+        in = assemble(data3);
+        out = NALReasemble.defragmentFUANals(in);
+
+        assertTrue("broken logic", out != null && out.length == 0);
+    }
+
+
+    @Test
+    public void testCorrectlyValidtesMultipleStartFUs()
+    {
+        byte[][] data = new byte[][] { new byte[] { (byte) 0x7C, (byte) 0x81 },
+                                       new byte[] { (byte) 0x7C, (byte) 0x81 } };
+
+        byte[] result = new byte[] { (byte) 0xE1 };
+
+        // Prepare & reassemble
+        final List<ByteBuffer> in = assemble(data);
+        final byte[] out = NALReasemble.defragmentFUANals(in);
+
+        assertTrue("broken logic", out != null && out.length > 0);
+        assertArrayEquals("bad reassembly", out, result);
+    }
+
+
+    @Test
+    public void testCorrectlyValidtesMissingEndFU()
+    {
+        byte[][] data = new byte[][] { new byte[] { (byte) 0x7C, (byte) 0x81 },
+                                       new byte[] { (byte) 0x7C, (byte) 0x01 } };
+
+        byte[] result = new byte[] { (byte) 0xE1 };
+
+        // Prepare & reassemble
+        final List<ByteBuffer> in = assemble(data);
+        final byte[] out = NALReasemble.defragmentFUANals(in);
+
+        assertTrue("broken logic", out != null && out.length > 0);
+        assertArrayEquals("bad reassembly", out, result);
+    }
+
+
+    @Test
+    public void testCorrectlyValidtesEndFUBeforeEnd()
+    {
+        byte[][] data = new byte[][] { new byte[] { (byte) 0x7C, (byte) 0x81, 0x01, 0x02 },
+                                       new byte[] { (byte) 0x7C, (byte) 0x01, 0x03, 0x04 },
+                                       new byte[] { (byte) 0x7C, (byte) 0x41, 0x05, 0x06 },
+                                       new byte[] { (byte) 0x7C, (byte) 0x41, 0x07, 0x08 } };
+
+        byte[] result = new byte[] { (byte) 0xE1, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 };
+
+        // Prepare & reassemble
+        final List<ByteBuffer> in = assemble(data);
+        final byte[] out = NALReasemble.defragmentFUANals(in);
+
+        assertTrue("broken logic", out != null && out.length > 0);
+        assertArrayEquals("bad reassembly", out, result);
+    }
+
+
+    @Test
+    public void testEmptyFUReassembleWorksStartAndEndOnly()
+    {
+        byte[][] data = new byte[][] { new byte[] { (byte) 0x7C, (byte) 0x81 },
+                                       new byte[] { (byte) 0x7C, (byte) 0x41 } };
+
+        byte[] result = new byte[] { (byte) 0x61 };
+
+        // Prepare & reassemble
+        final List<ByteBuffer> in = assemble(data);
+        final byte[] out = NALReasemble.defragmentFUANals(in);
+
+        assertTrue("broken logic", out != null && out.length > 0);
+        assertArrayEquals("bad reassembly", out, result);
+    }
+
+
+    @Test
+    public void testSimpleReassembleWorksStartAndEndOnly()
+    {
+        byte[][] data = new byte[][] { new byte[] { (byte) 0x7C, (byte) 0x81, 0x01, 0x02 },
+                                       new byte[] { (byte) 0x7C, (byte) 0x41, 0x03, 0x04 } };
+
+        byte[] result = new byte[] { (byte) 0x61, 0x01, 0x02, 0x03, 0x04 };
+
+        // Prepare & reassemble
+        final List<ByteBuffer> in = assemble(data);
+        final byte[] out = NALReasemble.defragmentFUANals(in);
+
+        assertTrue("broken logic", out != null && out.length > 0);
+        assertArrayEquals("bad reassembly", out, result);
+    }
+
+
+    @Test
+    public void testMultiPacketReassembleWorksStartAndEndOnly()
+    {
+        byte[][] data = new byte[][] { new byte[] { (byte) 0x7C, (byte) 0x81, 0x01, 0x02 },
+                                       new byte[] { (byte) 0x7C, (byte) 0x01, 0x03, 0x04 },
+                                       new byte[] { (byte) 0x7C, (byte) 0x01, 0x05, 0x06 },
+                                       new byte[] { (byte) 0x7C, (byte) 0x41, 0x07, 0x08 } };
+
+        byte[] result = new byte[] { (byte) 0x61, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };
+
+        // Prepare & reassemble
+        final List<ByteBuffer> in = assemble(data);
+        final byte[] out = NALReasemble.defragmentFUANals(in);
+
+        assertTrue("broken logic", out != null && out.length > 0);
+        assertArrayEquals("bad reassembly", out, result);
+    }
+
+
+    private List<ByteBuffer> assemble(final byte[][] data)
+    {
+        // JAVA8: return Arrays.asList(data).stream().map(b -> ByteBuffer.wrap(b)).collect(Collectors.toList());
+
+        final List<ByteBuffer> out = new ArrayList<ByteBuffer>(data.length);
+        for (int i = 0; i < data.length; i++)
+        {
+            out.add(ByteBuffer.wrap(data[i]));
+        }
+
+        return out;
+    }
+
 }
--- src/test/java/org/jcodec/codecs/h264/io/model/NalUnitTypeTest.java
+++ src/test/java/org/jcodec/codecs/h264/io/model/NalUnitTypeTest.java
@@ -4,47 +4,45 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class NalUnitTypeTest 
+public class NalUnitTypeTest
 {
 
     @Test
-    public void testAllValidNalTypesAreSupported() 
+    public void testAllValidNalTypesAreSupported()
     {
-    	assertEquals("type not supported as expected", NALUnitType.NON_IDR_SLICE, NALUnitType.fromValue(1));
-    	assertEquals("type not supported as expected", NALUnitType.SLICE_PART_A, NALUnitType.fromValue(2));
-    	assertEquals("type not supported as expected", NALUnitType.SLICE_PART_B, NALUnitType.fromValue(3));
-    	assertEquals("type not supported as expected", NALUnitType.SLICE_PART_C, NALUnitType.fromValue(4));
-    	assertEquals("type not supported as expected", NALUnitType.IDR_SLICE, NALUnitType.fromValue(5));
-    	assertEquals("type not supported as expected", NALUnitType.SEI, NALUnitType.fromValue(6));
-    	assertEquals("type not supported as expected", NALUnitType.SPS, NALUnitType.fromValue(7));
-    	assertEquals("type not supported as expected", NALUnitType.PPS, NALUnitType.fromValue(8));
-    	assertEquals("type not supported as expected", NALUnitType.ACC_UNIT_DELIM, NALUnitType.fromValue(9));
-    	assertEquals("type not supported as expected", NALUnitType.END_OF_SEQ, NALUnitType.fromValue(10));
-    	assertEquals("type not supported as expected", NALUnitType.END_OF_STREAM, NALUnitType.fromValue(11));
-    	assertEquals("type not supported as expected", NALUnitType.FILLER_DATA, NALUnitType.fromValue(12));
-    	assertEquals("type not supported as expected", NALUnitType.SEQ_PAR_SET_EXT, NALUnitType.fromValue(13));
-    	assertEquals("type not supported as expected", NALUnitType.AUX_SLICE, NALUnitType.fromValue(19));
-    	assertEquals("type not supported as expected", NALUnitType.FU_A, NALUnitType.fromValue(28));
+        assertEquals("type not supported as expected", NALUnitType.NON_IDR_SLICE, NALUnitType.fromValue(1));
+        assertEquals("type not supported as expected", NALUnitType.SLICE_PART_A, NALUnitType.fromValue(2));
+        assertEquals("type not supported as expected", NALUnitType.SLICE_PART_B, NALUnitType.fromValue(3));
+        assertEquals("type not supported as expected", NALUnitType.SLICE_PART_C, NALUnitType.fromValue(4));
+        assertEquals("type not supported as expected", NALUnitType.IDR_SLICE, NALUnitType.fromValue(5));
+        assertEquals("type not supported as expected", NALUnitType.SEI, NALUnitType.fromValue(6));
+        assertEquals("type not supported as expected", NALUnitType.SPS, NALUnitType.fromValue(7));
+        assertEquals("type not supported as expected", NALUnitType.PPS, NALUnitType.fromValue(8));
+        assertEquals("type not supported as expected", NALUnitType.ACC_UNIT_DELIM, NALUnitType.fromValue(9));
+        assertEquals("type not supported as expected", NALUnitType.END_OF_SEQ, NALUnitType.fromValue(10));
+        assertEquals("type not supported as expected", NALUnitType.END_OF_STREAM, NALUnitType.fromValue(11));
+        assertEquals("type not supported as expected", NALUnitType.FILLER_DATA, NALUnitType.fromValue(12));
+        assertEquals("type not supported as expected", NALUnitType.SEQ_PAR_SET_EXT, NALUnitType.fromValue(13));
+        assertEquals("type not supported as expected", NALUnitType.AUX_SLICE, NALUnitType.fromValue(19));
+        assertEquals("type not supported as expected", NALUnitType.FU_A, NALUnitType.fromValue(28));
     }
 
-    
+
     @Test
-    public void testReportsDataCorrectly() 
+    public void testReportsDataCorrectly()
     {
-    	assertEquals("name not as expected", NALUnitType.FU_A.getName(), "fragmented unit a");
-    	assertEquals("string not as expected", NALUnitType.FU_A.toString(), "FU_A");
-    	assertEquals("value not as expected", NALUnitType.FU_A.getValue(), 28);
+        assertEquals("name not as expected", NALUnitType.FU_A.getName(), "fragmented unit a");
+        assertEquals("string not as expected", NALUnitType.FU_A.toString(), "FU_A");
+        assertEquals("value not as expected", NALUnitType.FU_A.getValue(), 28);
     }
 
-    
-    
+
     @Test
-    public void testIdentifiesBadTypeCorrectly() 
+    public void testIdentifiesBadTypeCorrectly()
     {
-    	assertEquals("not as expected", NALUnitType.fromValue(1), NALUnitType.NON_IDR_SLICE);
-    	assertEquals("not as expected", NALUnitType.fromValue(100), null);
-    	assertEquals("not as expected", NALUnitType.fromValue(-1), null);
+        assertEquals("not as expected", NALUnitType.fromValue(1), NALUnitType.NON_IDR_SLICE);
+        assertEquals("not as expected", NALUnitType.fromValue(100), null);
+        assertEquals("not as expected", NALUnitType.fromValue(-1), null);
     }
 
-       
 }
