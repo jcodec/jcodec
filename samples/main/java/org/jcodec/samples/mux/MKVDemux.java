c107665542114836a9ac7c166f91b01acfdd53b1
Edouard DUPIN <yui.heero@gmail.com>
2020-12-17 15:37:44 +0100

    [DEV] sample MKVDemuxer: update display

--- samples/main/java/org/jcodec/samples/mux/MKVDemux.java
+++ samples/main/java/org/jcodec/samples/mux/MKVDemux.java
@@ -14,40 +14,52 @@ import org.jcodec.common.io.FileChannelWrapper;
 import org.jcodec.containers.mkv.MKVParser;
 import org.jcodec.containers.mkv.demuxer.MKVDemuxer;
 import org.jcodec.containers.mkv.demuxer.MKVDemuxer.AudioTrack;
+import org.jcodec.containers.mkv.demuxer.MKVDemuxer.SubtitlesTrack;
+import org.jcodec.containers.mkv.demuxer.MKVDemuxer.VideoTrack;
 import org.jcodec.containers.mkv.boxes.EbmlMaster;
 
 
 public class MKVDemux {
 
-	public static void main(String[] args) throws FileNotFoundException, IOException {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         if (args.length < 1) {
             System.out.println("Syntax: <in.mkv>\n" + "\tWhere:\n"
                     + "\t<in.mkv>\tThe file to decode");
             return;
         }
 
+        System.out.println("Read file: " + args[0]);
         FileInputStream demInputStream = new FileInputStream(args[0]);
         MKVDemuxer demuxer = new MKVDemuxer(new FileChannelWrapper(demInputStream.getChannel()));
-
-    	System.out.println("Audio tracks: " + demuxer.getAudioTracks().size());
+        System.out.println("Audio tracks: " + demuxer.getAudioTracks().size());
         for (DemuxerTrack elem : demuxer.getAudioTracks()) {
-        	AudioTrack elemAudio = (AudioTrack)elem;
-        	if (elemAudio == null) {
-        		System.out.println("    - null");
-        	} else {
-        		System.out.println("    - " + elemAudio.getLanguage() + "  " + elemAudio.getMeta().getCodec() + "  " + elem.getMeta().getAudioCodecMeta().getChannelCount() + "  " + elem.getMeta().getAudioCodecMeta().getSampleRate());
-        	}
+            AudioTrack elemAudio = (AudioTrack)elem;
+            if (elemAudio == null) {
+                System.out.println("    - null");
+            } else if (elemAudio.getMeta() == null) {
+                System.out.println("    - null (no metadata)");
+            } else {
+                System.out.println("    - " + elemAudio.getLanguage() + "  " + elemAudio.getMeta().getCodec() + "  " + elemAudio.getMeta().getAudioCodecMeta().getChannelCount() + "  " + elemAudio.getMeta().getAudioCodecMeta().getSampleRate());
+            }
         }
-    	System.out.println("Video tracks: " + demuxer.getVideoTracks().size());
+        System.out.println("Video tracks: " + demuxer.getVideoTracks().size());
         for (DemuxerTrack elem : demuxer.getVideoTracks()) {
-        	System.out.println("    - " + elem.getMeta().getType() + "  " + elem.getMeta().getCodec() + "  " + elem.getMeta().getVideoCodecMeta().getSize());
+            VideoTrack elemVideo = (VideoTrack)elem;
+            System.out.println("    - " + elemVideo.getMeta().getType() + "  " + elemVideo.getMeta().getCodec() + "  " + elemVideo.getMeta().getVideoCodecMeta().getSize());
         }
-    	System.out.println("Video subtitle tracks: " + demuxer.getSubtitleTracks().size());
+        System.out.println("Video subtitle tracks: " + demuxer.getSubtitleTracks().size());
         for (DemuxerTrack elem : demuxer.getSubtitleTracks()) {
-        	System.out.println("    - " + elem.getMeta().getType() + "  " + elem.getMeta().getCodec());
+            SubtitlesTrack elemSubtitle = (SubtitlesTrack)elem;
+            if (elemSubtitle == null) {
+                System.out.println("    - null");
+            } else if (elemSubtitle.getMeta() == null) {
+                System.out.println("    - null (no metadata)");
+            } else {
+                   System.out.println("    - " + elemSubtitle.getLanguage() + "  " + elemSubtitle.getMeta().getType() + "  " + elemSubtitle.getMeta().getCodec());
+            }
         }
         closeQuietly(demInputStream);
         
-	}
+    }
 
 }
