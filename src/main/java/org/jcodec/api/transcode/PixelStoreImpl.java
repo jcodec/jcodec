3c093f16f13982e3ca9cadb9b82a3983b306ac05
Gabor Kecskemeti (sh) <gabor@simple-help.com>
2022-07-22 11:36:41 +0100

    PixelStore is now bounded to avoid memory issues

--- src/main/java/org/jcodec/api/transcode/PixelStoreImpl.java
+++ src/main/java/org/jcodec/api/transcode/PixelStoreImpl.java
@@ -2,22 +2,33 @@ package org.jcodec.api.transcode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.jcodec.common.model.ColorSpace;
 import org.jcodec.common.model.Picture;
 
 public class PixelStoreImpl implements PixelStore {
+    private Random rnd = new Random();
+    private int bound;
     private List<Picture> buffers;
 
     public PixelStoreImpl() {
         buffers = new ArrayList<Picture>();
+        bound = 10;
+    }
+
+    public int getBound() {
+        return bound;
+    }
+
+    public void setBound(int bound) {
+        this.bound = bound;
     }
 
     @Override
     public LoanerPicture getPicture(int width, int height, ColorSpace color) {
         for (Picture picture : buffers) {
-            if (picture.getWidth() == width && picture.getHeight() == height
-                    && picture.getColor() == color) {
+            if (picture.getWidth() == width && picture.getHeight() == height && picture.getColor() == color) {
                 buffers.remove(picture);
                 return new LoanerPicture(picture, 1);
             }
@@ -31,7 +42,11 @@ public class PixelStoreImpl implements PixelStore {
         if (frame.unused()) {
             Picture pixels = frame.getPicture();
             pixels.setCrop(null);
-            buffers.add(pixels);
+            if (buffers.size() == bound) {
+                buffers.set(rnd.nextInt(bound), pixels);
+            } else {
+                buffers.add(pixels);
+            }
         }
     }
 
